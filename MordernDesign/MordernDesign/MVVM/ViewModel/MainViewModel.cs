9f2b0d53ff07398e2c958e438a9bebcc050c30a0
Munnyz <65402631+Munnyz@users.noreply.github.com>
2021-08-30 16:45:55 -0300

    Add files via upload

---
+++ MordernDesign/MordernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using MordernDesign.Core;
+using System;
+
+
+namespace MordernDesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand SobreViewCommand { get; set; }
+        public RelayCommand ContatoViewCommand { get; set; }
+        public HomeViewModel HomeVm { get; set; }
+        public SobreViewModel SobreVm { get; set; }
+        public ContatoViewModel ContatoVm { get; set; }
+
+        private object _CurrentView;
+
+        public object CurrentView
+        {
+            get { return _CurrentView; }
+            set { 
+                _CurrentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            SobreVm = new SobreViewModel();
+            ContatoVm = new ContatoViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVm;
+            });
+            SobreViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SobreVm;
+            });
+            ContatoViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ContatoVm;
+            });
+
+        }
+    }
+}
